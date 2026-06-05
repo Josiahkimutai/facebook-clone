67f88efc433d29b34055973d9fb05ba4e28149aa
Josiah Kimutai <josiahkimutai78@gmai.com>
2026-06-05 20:24:04 +0300

    Facebook style auth

--- app.js
+++ app.js
@@ -4,20 +4,38 @@ const supabaseUrl = 'https://cpmzvqcoiknotpcutljh.supabase.co'
 
 const supabaseKey = 'sb_publishable_iWUiLtL50Ptcm5sYVBUSvA_U7WyfV-f'
 
+
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-window.signUp = async function () {
+window.signup = async function () {
 
+    const username = document.getElementById('username').value
     const email = document.getElementById('email').value
     const password = document.getElementById('password').value
 
-    const { error } = await supabase.auth.signUp({
+    const { data, error } = await supabase.auth.signUp({
         email,
         password
     })
 
     if (error) {
         alert(error.message)
+        return
+    }
+
+    const user = data.user
+
+    const { error: profileError } = await supabase
+        .from('profiles')
+        .insert([
+            {
+                id: user.id,
+                username: username
+            }
+        ])
+
+    if(profileError){
+        alert(profileError.message)
     } else {
         alert("Signup successful")
     }
@@ -33,81 +51,12 @@ window.login = async function () {
         password
     })
 
-    if (error) {
+    if(error){
         alert(error.message)
     } else {
+
         alert("Login successful")
+
+        window.location.href = "home.html"
     }
 }
-
-window.logout = async function () {
-
-    await supabase.auth.signOut()
-
-    alert("Logged out")
-}
-
-window.createPost = async function () {
-
-    const content = document.getElementById('postContent').value
-
-    if (!content) {
-        alert("Write something")
-        return
-    }
-
-    const user = await supabase.auth.getUser()
-
-    if (!user.data.user) {
-        alert("Please login first")
-        return
-    }
-
-    const { error } = await supabase
-        .from('posts')
-        .insert([{
-            content
-        }])
-
-    if (error) {
-
-        alert("Post failed")
-        console.log(error)
-
-    } else {
-
-        alert("Post Success")
-
-        document.getElementById('postContent').value = ''
-
-        loadPosts()
-    }
-}
-
-async function loadPosts() {
-
-    const { data, error } = await supabase
-        .from('posts')
-        .select('*')
-        .order('id', { ascending: false })
-
-    if (error) {
-        console.log(error)
-        return
-    }
-
-    const postsDiv = document.getElementById('posts')
-
-    postsDiv.innerHTML = ''
-
-    data.forEach(post => {
-
-        postsDiv.innerHTML += `
-            <div class="post">
-                ${post.content}
-            </div>
-        `
-    })
-}
-
-loadPosts()
