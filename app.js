3c82573c50975fb6f88601e0f189538cf5af63ea
Josiah Kimutai <josiahkimutai78@gmai.com>
2026-06-05 19:35:11 +0300

    Added login system

--- app.js
+++ app.js
@@ -1,22 +1,73 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js'
 
 const supabaseUrl = 'https://cpmzvqcoiknotpcutljh.supabase.co'
+
 const supabaseKey = 'sb_publishable_iWUiLtL50Ptcm5sYVBUSvA_U7WyfV-f'
 
 const supabase = createClient(supabaseUrl, supabaseKey)
+
+window.signUp = async function () {
+
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+
+    const { error } = await supabase.auth.signUp({
+        email,
+        password
+    })
+
+    if (error) {
+        alert(error.message)
+    } else {
+        alert("Signup successful")
+    }
+}
+
+window.login = async function () {
+
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+
+    const { error } = await supabase.auth.signInWithPassword({
+        email,
+        password
+    })
+
+    if (error) {
+        alert(error.message)
+    } else {
+        alert("Login successful")
+    }
+}
+
+window.logout = async function () {
+
+    await supabase.auth.signOut()
+
+    alert("Logged out")
+}
 
 window.createPost = async function () {
 
     const content = document.getElementById('postContent').value
 
     if (!content) {
-        alert("Please write something")
+        alert("Write something")
+        return
+    }
+
+    const user = await supabase.auth.getUser()
+
+    if (!user.data.user) {
+        alert("Please login first")
         return
     }
 
     const { error } = await supabase
         .from('posts')
-        .insert([{ content }])
+        .insert([{
+            content
+        }])
 
     if (error) {
 
@@ -38,7 +89,7 @@ async function loadPosts() {
     const { data, error } = await supabase
         .from('posts')
         .select('*')
-        .order('created_at', { ascending: false })
+        .order('id', { ascending: false })
 
     if (error) {
         console.log(error)
